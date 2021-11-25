4752e9b676c87b1d04637f64f57453bcb1296781
dodogabrie <dodogabrie97@live.it>
2021-11-25 16:07:04 +0100

    create an ingenuos test for k-fold

--- test/test_split.py
+++ test/test_split.py
@@ -26,10 +26,22 @@ class TestSplit(unittest.TestCase):
             check_shuffled += (list(v) == data_list[j]) # // // //
 
         self.assertEqual(check_all_in, len(dataset), 'Missing some data!') # Returned all the data
-        self.assertGreater(len(dataset), check_shuffled, 'Data are not shuffled!') # Returned all the data
-        # testing if the returned data respect the proportions
+        self.assertGreater(len(dataset), check_shuffled, 'Data are not shuffled!')
         self.assertEqual(len(train), round(frac_train * len(dataset)), 'Proportion of train not respected' )
-        self.assertEqual(len(val), round((1-frac_train) * len(dataset)), 'proportion of validationi')
+        self.assertEqual(len(val), round((1-frac_train) * len(dataset)), 'proportion of validation not respected')
+
+    def test_kfold(self):
+        k = 3
+        dataset = np.arange(20).reshape((10,2))
+        train, idxs = pp.split(dataset, kind = 'k-fold', k = k, shuffle = False)
+        # To remember who to split with indexs of idxs...
+        #for idx1, idx2 in idxs:
+        #    print('train set')
+        #    print(np.delete(train, slice(idx1, idx2), axis = 0))
+        #    print('validation set')
+        #    print(train[idx1:idx2])
+        #    print('\n')
+        self.assertEqual(len(idxs), k, 'Returned wrong number of splits') # Just a stupid test
 
 if __name__ == '__main__':
     unittest.main()
