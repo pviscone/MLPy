1617d3f83d9afb5b863e1b694c00dbb32c3c440d
dodogabrie <dodogabrie97@live.it>
2021-11-25 16:05:30 +0100

    added k-fold splitting

--- NN/utils/preprocessing.py
+++ NN/utils/preprocessing.py
@@ -25,8 +25,18 @@ def split(input_matrix, frac_training=0.8, shuffle=False, kind="hold_out", k=4):
 
     Returns
     -------
-    (numpy 2d array, numpy 2d array)
-        (train data matrix, validation data matrix)
+    (numpy 2d array, numpy 2d array) if kind = 'hold-out'
+    (numpy 2d array,  list of tuple) if kind = 'k-fold'
+        If hold out return (train data matrix, validation data matrix).
+        If k-fold return (data matrix, list of tuple containing 2 indexs).
+        The index in the list represent the (idx1 = start, idx2 = stop) index
+        to split for each fold. In order to not waste memory you can separate
+        with this 2 index each train-val dataset berore training the model.
+        Note:
+            to extract validation set just use the slicing: data[idx1, idx2]
+            to extract train set just use
+            numpy.delete(data, slice(idx1,idx2), axis = 0)
+
     """
     copy_data = np.copy(input_matrix) # Copy the dataset to not change original
                                       # input_matrix
@@ -38,4 +48,10 @@ def split(input_matrix, frac_training=0.8, shuffle=False, kind="hold_out", k=4):
         validation_set=copy_data[idx:,:]
         return training_set, validation_set
     elif kind=="k-fold":
-        pass
+        n_data = len(copy_data)
+        fold_size = np.floor(len(copy_data)/k)
+        upper_bound = lambda x: x if n_data-x > fold_size else n_data
+        idxs = [(int(i*fold_size),
+                 int(upper_bound((i+1)*fold_size)))
+                for i in range(k)]
+        return copy_data, idxs
