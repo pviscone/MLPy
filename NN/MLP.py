ce8f6fcd9307615f4eea8cc62b51e00e796b3e80
dodogabrie <dodogabrie97@live.it>
2021-11-29 16:20:05 +0100

    Modified from txt to json for network saving

--- NN/MLP.py
+++ NN/MLP.py
@@ -3,6 +3,8 @@ Implementation of the Multi Layer Perceptron.
 """
 import time
 import os
+from pathlib import Path
+import json
 import numpy as np
 #from numba import njit # <---- Decomment for a 5x speed up
 from utils.losses import MEE_MSE
@@ -13,7 +15,8 @@ class MLP:
     Multi Layer Perceptron class.
     """
     def __init__(self, structure = [], func = None, starting_points = 0.1,
-                 eta=0.1, lamb=0, norm_L=2, alpha=0, nesterov=False, directory_name = None):
+                 eta=0.1, lamb=0, norm_L=2, alpha=0, nesterov=False, 
+                 filename = None, epoch_to_restore = -1):
         """
         __init__ function of the class.
 
@@ -41,62 +44,48 @@ class MLP:
             The starting weights of each layer (i) is initialized extracting
             from a random uniform distribution in the interval:
                 [-starting_point[i], starting_point[i]]
+        filename : string
+            Restore the net from a json file 'filename', if None initialize a
+            new net.
+        epoch_to_restore : int
+            Number of epoch to restore (if want to reload a pretrained net).
+            If -1 reload the network fully trained.
         """
-        self.directory_name = directory_name
-        if self.directory_name == None:
-            self.structure=structure #numero di unità per ogni layer
+        self.filename = filename
+        self.network=[]
+        if self.filename == None: # If the user doesn't want to restore a previous net
+            self.structure=structure # Number of units per layer
             # list of tuple with (function, parameter of funtcion)
             self.func=[f if isinstance(f, (tuple, list)) else (f, 1) for f in func]
-            self.starting_points=starting_points #lista degli start_point
-            self.eta = eta
-            self.lamb = lamb
-            self.norm_L = norm_L
-            self.alpha = alpha
-            self.nesterov = nesterov
-        else:
-            if self.directory_name[-1] != '/': self.directory_name += '/'
-            with open(self.directory_name + 'net_info', 'r') as f:
-                for line in f:
-                    attr = line.split(' -> ')[0][2:]
-                    raw_val = line.split(' -> ')[1][:-1]
-                    if attr == 'structure':
-                        struct = raw_val[1:-1].split(', ')
-                        val = [int(num) for num in struct]
-                    elif attr == 'func':
-                        val = []
-                        funcs = raw_val[1:-2].split('), ')
-                        for fs in funcs:
-                            f_name = fs.split(', ')[0][2:-1]
-                            f_param = int(fs.split(', ')[1])
-                            tuple_f = (f_name, f_param)
-                            val.append(tuple_f)
-                    elif attr == 'starting_points':
-                        struct = raw_val[1:-1].split(', ')
-                        val = [float(num) for num in struct]
-                    elif attr == 'nesterov':
-                        if line.split(' ->')[1][:-1] == 'False':
-                            val = False
-                        elif line.split(' ->')[1][:-1] == 'True':
-                            val = True
-                        else: val = False
-                    else:
-                        val = float(line.split(' ->')[1])
-                    setattr(self, attr, val)
-
-        self.network=[]
-        self.train_MEE = []
-        self.train_MSE = []
-        self.val_MEE = []
-        self.val_MSE = []
-        self.epoch = 0
-
+            self.starting_points=starting_points # start_point list for random weights
+            self.eta = eta # Learning rate
+            self.lamb = lamb # Tikhonov regularization
+            self.norm_L = norm_L # Tikhonov regularization norm
+            self.alpha = alpha # Parameter for momentum
+            self.nesterov = nesterov # If the user want the nesterov momentum
+            self.epoch = 0 # set number of epoch to 0
+            self.train_MEE = []
+            self.train_MSE = []
+            self.val_MEE = []
+            self.val_MSE = []
+        else: # If the user want to load a pretrained model
+            with open(self.filename) as json_file: # open the file with the json net
+                data = json.load(json_file) # Load the file in a dictionary
+                for key, val in data.items(): # Create all the attribute from file
+                    if ('weights' not in key) and ('bias' not in key):
+                        setattr(self, key, val)
+            # If epoch_to_restore = -1 restore the last epoch
+            if epoch_to_restore == -1: self.epoch_to_restore = self.epoch
+            else: self.epoch_to_restore = epoch_to_restore
+            # Create a net with empty input/val just to be able to predict without training
+            self.create_net(np.empty(data['input_data_shape']), np.empty(data['val_data_shape']))
 
     def __getattr__(self,attr):
         """Get the atribute of MLP"""
         return [getattr(lay,attr) for lay in self.network]
 
     def train(self, input_data, labels, val_data, val_labels, epoch,
-                clean_net = False):
+              clean_net = False, save_rate = -1, filename = None):
         """
         Parameters
         ----------
@@ -112,6 +101,8 @@ class MLP:
             Labels of the val_data.
         clean_net : boolean
             If True restore the net, if False keep the pretrained net (if exist)
+        save_rate : int
+            Rate of saving of the net during the training
         """
         # Check if all the input are numpy arrays
         input_data = np.array(input_data)
@@ -129,7 +120,11 @@ class MLP:
 
         # If the net is just an empty list fill it with the layers
         if len(self.network) == 0:
-            self.create_net(input_data,val_data)
+            self.create_net(input_data, val_data)
+
+        if self.filename != None: # If is a preloaded net fill the input
+            self.network[0].input = input_data
+            self.network[0].val_data = val_data
 
         # Start train the net
         total_time = 0
@@ -152,8 +147,8 @@ class MLP:
             self.val_MSE.append(MSE)
 
             # Printing the error
-            string_val_err = f'  [val MEE = {self.val_MEE[self.epoch]:.4f}]'
-            string_err = f'  [train MEE = {self.train_MEE[self.epoch]:.4f}]'
+            string_val_err = f'  [val MEE = {self.val_MEE[-1]:.4f}]'
+            string_err = f'  [train MEE = {self.train_MEE[-1]:.4f}]'
             string_err += string_val_err
 
             # Printing remaining time
@@ -163,6 +158,9 @@ class MLP:
             remain_time = mean_for_loop*(epoch-i)
             string_time = f'  [wait {remain_time:.1f} s]'
             print(f'[Epoch {self.epoch}]' + string_err + string_time + ' '*10, end = '\r', flush = True)
+
+            if i == save_rate:
+                self.save_network(filename)
 
             # Updating epoch
             self.epoch += 1
@@ -189,7 +187,7 @@ class MLP:
         self.feedforward()
         return self.network[-1].out
 
-    def create_net(self, input_data, val_data):
+    def create_net(self, input_data, val_data, w_list = [], bias_list = []):
         """
         Feed the input of the net and propagate it
 
@@ -198,35 +196,30 @@ class MLP:
         input_data : list or numpy 2d array
             Array with the data in input to the MLP.
         """
-        if self.directory_name == None:
-            for layer,num_unit in enumerate(self.structure):
-                if layer==0: #If empty, initializing the neural network
-                    self.network.append(Layer(num_unit,input_data,
-                                        val_matrix=val_data,
-                                        func=self.func[layer],
-                                        starting_points=self.starting_points[layer]))
-                else:
-                    self.network.append(Layer(num_unit,self.network[layer-1].out, 
-                                        val_matrix=self.network[layer-1].out_val,
-                                        func=self.func[layer],
-                                        starting_points=self.starting_points[layer]))
+        if self.filename == None: 
+            from_backup = False
+            w, bias = [None]*len(self.structure), [None]*len(self.structure)
         else:
-            for layer,num_unit in enumerate(self.structure):
-                bias_w = np.loadtxt(directory_name + f'layer{layer}.txt')
-                bias = bias_w[:, 0]
-                w = bias_w[:, 1:]
-                if layer==0: #If empty, initializing the neural network
-                    self.network.append(Layer(num_unit,input_data,
-                                        val_matrix=val_data,
-                                        func=self.func[layer],
-                                        preload_w = w,
-                                        preload_bias = bias))
-                else:
-                    self.network.append(Layer(num_unit,self.network[layer-1].out, 
-                                        val_matrix=self.network[layer-1].out_val,
-                                        func=self.func[layer],
-                                        preload_w = w,
-                                        preload_bias = bias))
+            from_backup = True
+            with open(self.filename, 'r') as json_file:
+                data = json.load(json_file)
+                w = data[f'weights_epoch{self.epoch_to_restore}']
+                bias = data[f'bias_epoch{self.epoch_to_restore}']
+        for layer,num_unit in enumerate(self.structure):
+            if layer==0: 
+                self.network.append(Layer(num_unit,input_data,
+                                    val_matrix=val_data,
+                                    func=self.func[layer],
+                                    preload_w = w[layer],
+                                    preload_bias = bias[layer], 
+                                    from_backup = from_backup))
+            else:
+                self.network.append(Layer(num_unit,self.network[layer-1].out, 
+                                    val_matrix=self.network[layer-1].out_val,
+                                    func=self.func[layer],
+                                    preload_w = w[layer],
+                                    preload_bias = bias[layer],
+                                    from_backup = from_backup))
 
 
     def feedforward(self):
@@ -280,16 +273,44 @@ class MLP:
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-    def save_network(self, directory_name):
-        net_dict = self.__dict__
-        skip_list = ['network', 'directory_name', 'train_MEE', 'train_MSE', 
-                     'val_MEE', 'val_MSE', 'epoch']
+    def save_network(self, filename):
+        """
+        Function that save the net in a json file
+
+        Parameters
+        ----------
+        filename : string
+            The output filename.
+        """
         # Write general info of the net
-        if not os.path.exists(directory_name):
-            os.makedirs(directory_name)
-        with open(directory_name + 'net_info', 'w') as f:
+        path_file = Path(filename) # Create a path object
+        if not os.path.exists(path_file.parent): # If the folder not exist create it
+            os.makedirs(path_file.parent)
+        if not path_file.is_file(): # If the file doesn't exist initialize 
+#                                   # the net parameters
+            net_dict = self.__dict__ # Dict of the MLP object
+            # Stuff that we don't write to the output file
+            skip_list = ['network', 'filename']
+            # (network will be written below)
+            save_dict = {} # initialize the dict to write
+            save_dict['input_data_shape'] = self.network[0].input.shape
+            save_dict['val_data_shape'] = self.network[0].val_data.shape
             for net_attr in net_dict.keys():
-                if net_attr not in skip_list:
-                    f.write(f'# {net_attr} -> {net_dict[net_attr]}\n')
+                if net_attr not in skip_list: # Write the values in the dict
+                    save_dict[net_attr] = net_dict[net_attr]
+        else: # If the file exist (like when we save during the training)
+            with open(filename, 'r') as json_file:
+                save_dict = json.load(json_file) # Just load the json and go on
+
+        # Saving the weights and the bias for each layer
+        weights_list = []
+        bias_list = []
         for i, layer in enumerate(self.network):
-            np.savetxt(directory_name + f'layer{i}.txt', np.c_[layer.bias, layer.weight])
+            weights_list.append(layer.weight.tolist())
+            bias_list.append(layer.bias.tolist())
+        save_dict[f'weights_epoch{self.epoch}'] = weights_list
+        save_dict[f'bias_epoch{self.epoch}'] = bias_list
+
+        # Save the output file
+        with open(filename, 'w') as outfile:
+            json.dump(save_dict, outfile)
