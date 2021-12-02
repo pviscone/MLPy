d1cb373665cb9dff0d6b45fdefe998b39a97916c
dodogabrie <dodogabrie97@live.it>
2021-12-02 09:05:38 +0100

    Added quick prop learning algorithm and improved stop criteria

--- NN/CC.py
+++ NN/CC.py
@@ -1,6 +1,7 @@
 """
 Implementation of the Multi Layer Perceptron.
 """
+
 import time
 import numpy as np
 #from numba import njit # <---- Decomment for a 5x speed up
@@ -12,7 +13,7 @@ class Neuron:
     """
     Neuron of the Cascade Correlation Neural Network.
     """
-    def __init__(self, data_shape, func = ("sigmoid",1), w_init = 0.1, connect = True):
+    def __init__(self, data_shape, func = ("sigmoid",1), w_init = 0.1):
         n_data, n_prev_neurons = data_shape
         self.weight=np.random.uniform(-w_init,w_init, size = n_prev_neurons )
         # Array of bias for each unit in the Layer
@@ -22,7 +23,6 @@ class Neuron:
         self.function, self.slope=func
         self.func=lambda x : actv_funcs(self.function)(x,self.slope)
         self.der_func=lambda x : dactv_funcs(self.function)(x,self.slope)
-        self.connect = connect
 
     def net(self, input_data):
         return input_data.dot(self.weight) + self.bias
@@ -61,7 +61,7 @@ class CCNN:
     def out_net(self):
         return np.array([neu.out(self.transfer_line) for neu in self.out_neurons]).T
 
-    def train(self, input_data, labels, min_epoch = 10, max_hidden = 0, 
+    def train(self, input_data, labels, min_epoch = 10, max_epoch = 1000, max_hidden = 0, 
               stop_threshold = 0.1, stack_threshold = 0.1,
               n_candidate = 10, candidate_epoch = 100):
         if self.num_input == 0:
@@ -73,8 +73,9 @@ class CCNN:
             relative_delta_E = 2*stop_threshold
             sum_delta_E = 0
             if add_neuron:
-                print('I will add an hidden')
+                print('add hidden')
                 self.add_hidden_neuron(labels, n_candidate = n_candidate, candidate_epoch = candidate_epoch)
+                print('done')
             while (relative_delta_E > stack_threshold):# and epoch < max_epoch):
                 self.feedforward()
                 self.output_learning_step(labels)
@@ -82,53 +83,94 @@ class CCNN:
                 if epoch > 0:
                     delta_E = self.train_MSE[-1] - MSE
                     sum_delta_E += delta_E
-                if epoch > min_epoch:
-                    mean_delta_E = sum_delta_E/epoch
-                    relative_delta_E = delta_E/mean_delta_E
+                    if epoch < min_epoch:
+                        mean_delta_E = sum_delta_E/epoch
+                    else:
+                        relative_delta_E = delta_E/mean_delta_E
                 self.train_MEE.append(MEE)
                 self.train_MSE.append(MSE)
                 epoch += 1
+                self.epoch+=1
+                if MSE < stop_threshold: break
+                if self.epoch > max_epoch : break
             if MSE < stop_threshold: break
+            if self.epoch > max_epoch: break
             else: add_neuron = True
 
     def add_hidden_neuron(self, labels, n_candidate = 50, candidate_epoch = 100):
+        transfer_data_shape = self.transfer_line.shape
+        out_net = self.out_net
+        E = out_net - labels
+        delta_E = E - np.mean(E, axis = 0)
+
         def generate_neuron():
-            return Neuron(data_shape = self.transfer_line.shape,
+            return Neuron(data_shape = transfer_data_shape,
                           func = self.hidden_actv_f, 
-                          w_init = self.w_init, connect = False)
+                          w_init = self.w_init)
+
         def compute_S(labels, candidate):
             S = 0
             for k in range(self.num_output):
                 o_p = candidate.out(self.transfer_line)
-                E_pk = self.out_net[:,k] - labels[:,k]
+                E_pk = out_net[:,k] - labels[:,k]
                 meanE_pk = np.mean(E_pk)
                 delta_E = E_pk - meanE_pk
-                S_k = np.sum((o_p - np.mean(self.out_net))*delta_E)
+                S_k = np.abs(np.sum((o_p - np.mean(out_net))*delta_E))
                 S += S_k
             return S
+
+        def gradient(labels, candidate):
+            o_p = candidate.out(self.transfer_line)
+            dnet = candidate.der_func(candidate.net(self.transfer_line))
+            w_grad = np.zeros(candidate.weight.shape)
+            b_grad = 0
+            for k in range(self.num_output):
+                S_k = np.sum((o_p - np.mean(out_net))*delta_E[:,k])
+                delta = np.sign(S_k) * delta_E[:,k] * dnet
+                w_grad += np.sum([i*j for i,j in zip(delta, self.transfer_line)], axis=0)
+                b_grad += np.sum(delta) 
+            return w_grad, b_grad
+
+        def grad_desc_step(labels, candidate):
+            w_grad, b_grad = gradient(labels, candidate)
+            candidate.weight += self.eta*w_grad
+            candidate.bias   += self.eta*b_grad
+            return w_grad, b_grad
+
+        def quick_prop_step(labels, candidate, w_g_prev, b_g_prev):
+            w_grad, b_grad = gradient(labels, candidate)
+            dw_prev = self.eta * w_g_prev
+            db_prev = self.eta * b_g_prev
+            dw = dw_prev * w_grad / (w_g_prev - w_grad)
+            db = db_prev * b_grad / (b_g_prev - b_grad)
+            candidate.weight += w_grad
+            candidate.bias   += b_grad
+            return w_grad, b_grad
 
         candidate = generate_neuron()
         best_neu = candidate
         best_S = 0
         for i in range(n_candidate):
+            w_g_prev, b_g_prev = grad_desc_step(labels, candidate)
             for j in range(candidate_epoch):
-                self.feedforward()
-                self.hidden_learning_step(labels, candidate)
+                w_g_prev, b_g_prev = quick_prop_step(labels, candidate, w_g_prev, b_g_prev)
             S = compute_S(labels, candidate)
             if S > best_S:
                 best_S = S
                 best_neu = candidate
             candidate = generate_neuron()
 
+        start = time.time()
         self.hid_neurons.append(best_neu)
 
         out_new_neu = self.hid_neurons[-1].out(self.transfer_line)
 
-        n_data = len(self.transfer_line)
+        n_data = transfer_data_shape[0]
         self.transfer_line = np.column_stack((self.transfer_line, out_new_neu))
         for out in self.out_neurons:
             out.weight = np.append(out.weight, np.random.uniform(-self.w_init, self.w_init))
         self.num_hidden += 1
+        elapsed = time.time()-start
 
     def create_net(self, input_data, n_output):
         self.num_output = n_output
@@ -158,25 +200,9 @@ class CCNN:
             neu_out.weight += self.eta*dW
             neu_out.bias   += self.eta*db
 
-    def hidden_learning_step(self, labels, candidate):
-        o_p = candidate.out(self.transfer_line)
-        dnet = candidate.der_func(candidate.net(self.transfer_line))
-        dW = np.zeros(len(candidate.weight))
-        for k in range(self.num_output):
-            E_pk = self.out_net[:,k] - labels[:,k]
-            meanE_pk = np.mean(E_pk)
-            delta_E = E_pk - meanE_pk
-            S_k = np.sum((o_p - np.mean(self.out_net))*delta_E)
-            delta = np.sign(S_k) * delta_E * dnet
-            dW = np.sum([i*j for i,j in zip(delta, self.transfer_line)], axis=0)
-            db = np.sum(delta) 
-            candidate.weight += self.eta * dW
-            candidate.bias += self.eta * db
-
     def predict(self, data):
         n_data, n_features = data.shape
         self.transfer_line = np.ones((n_data, n_features + self.num_hidden))
-        print(self.transfer_line[:, :n_features].shape)
         self.transfer_line[:, :n_features] = data
         self.feedforward()
         return self.out_net
