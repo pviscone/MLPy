b3c6d90b2ec6f206229c8b1f06a4e8a883798775
dodogabrie <dodogabrie97@live.it>
2021-12-11 10:06:54 +0100

    added more documentation and created S_list, epoch_trained

--- NN/neuron_CC.py
+++ NN/neuron_CC.py
@@ -6,9 +6,32 @@ class Neuron:
     Neuron of the Cascade Correlation Neural Network.
     """
     def __init__(self, data_shape, func = ("sigmoid",1), w_init = 0.1):
+        """
+        __init__ function of the Neuron.
+
+        Parameters
+        ----------
+        data_shape : tuple or list
+            Tuple or list contaning the data shape coming from the inputs 
+            or from both inputs and the previous hidden.
+        func : tuple
+            Tuple containing a string with the activation function name
+                - sigmoid
+                - linear
+                - relu
+            and the parameter of the relative function (decay for the sigmoid, 
+            angular coefficient for the linear/relu).
+        w_init : float
+            the weights will be sampled from a random uniform distribution in 
+            [-w_init, w_init], also the bias will be extracted in this interval.
+        """
+        # Separate number of data and number of previous neuron (considering 
+        # inputs as neurons)
         n_data, n_prev_neurons = data_shape
+
+        # Initialize the weights
         self.weight=np.random.uniform(-w_init,w_init, size = n_prev_neurons )
-        # Array of bias for each unit in the Layer
+        # Initialize the bias (just a number...)
         self.bias=np.random.uniform( -w_init, w_init )
 
         #Storing the activation function and his derivative
@@ -16,8 +39,15 @@ class Neuron:
         self.func=lambda x : actv_funcs(self.function)(x,self.slope)
         self.der_func=lambda x : dactv_funcs(self.function)(x,self.slope)
 
+        # Initialize two attribute for the history of hidden unit during 
+        # their training
+        self.S_list = []
+        self.epoch_trained = 0
+
     def net(self, input_data):
+        """Net function for the neuron"""
         return input_data.dot(self.weight) + self.bias
 
     def out(self, input_data):
+        """Out function for the neuron"""
         return self.func(self.net(input_data))
