940f6f4eeb6dd7705eb8af31a9e9043f1b5b185f
dodogabrie <dodogabrie97@live.it>
2021-12-16 18:19:29 +0100

    compacted code and add new comments

--- NN/utils/grid_search.py
+++ NN/utils/grid_search.py
@@ -52,52 +52,54 @@ def grid_search(model, dict_models, dict_params, input_data, labels, val_data, v
         - 'model': the parameters of the model.
         - 'model_name': the name of the model given in dict_models.
     """
-    list_results = []
     # Extract the values of the dictionary
     list_params_values = list(dict_params.values())
-    # Exract the keys
-    list_params_keys = list(dict_params.keys())
     # Create a list with all the permutations of the values
     list_permutation = list(itertools.product(*list_params_values))
-    list_candidates = [] # Initialize an empty list for the final result
-    # This loop is for flattening all the candidate in an unique list 
-    # (to perform an easy sort on error then).
-    for mod_name, mod_params in dict_models.items():
+    list_candidates = [] # Initialize an empty list for save all the candidates
+    list_results = [] # List for final sorting of candidates based on error
+    i = 0 # incremental variable for double loop
+    n_candidates = len(list_permutation)*len(dict_models) # num candidates
+    err_val_array = np.empty(n_candidates) # Empty array for the errors
+    start = time.time() # Time counter
+    for mod_name, mod_params in dict_models.items(): # Apply the combination to
+        #                                            # each model
         for params in list_permutation: # For each combination of parameter
-            # Create a dictionary that recreate the structure of the input 
-            # to the model: {'p1':p1_val, 'p2':p2_val, ...}
-            dict_candidate = {}
-            # Fill the train parameters
-            dict_candidate['train'] = {k:p for k, p in zip(list_params_keys, params)}
+
+            ######## Create the dictionary with parameters of candidate #######
+
+            dict_candidate = {} # Fill with the info for each candidate model
+            # Fill the train parameters (like {'p1':p1_val, 'p2':p2_val, ...} )
+            dict_candidate['train'] = {k:p for k, p in zip(dict_params.keys(), params)}
             dict_candidate['model'] = mod_params # set the model parameters
             dict_candidate['model_name'] = mod_name # and the model name 
-            # Append that dictionary to the final list
-            list_candidates.append(dict_candidate)
-       
-    n = len(list_candidates) # Number of candidates
-    err_val_array = np.empty(n) # Empty array for the errors
-    start = time.time() # Time counter
-    for i, dict_params in enumerate(list_candidates): # for each candidate
-        mod_params = dict_params['model'] # Extract model params'
-        mod_name = dict_params['model_name'] # Extract model name
-        candidate = model(**mod_params) # Create the candidate
-        if verbose: # Print only if verbose
-            print(f"{i} on {n}: Model {mod_name} --> Train parameters: {dict_params['train']}")
-        # Train the candidate with the parameters in dict_params['train']
-        candidate.train(input_data, labels, val_data, val_labels, **dict_params['train'])
-        # predict on validation data
-        pred_val = candidate.predict(val_data)
-        # add the error to the list of error to position i
-        err_val_array[i] = error(val_labels, pred_val)
-        print('\n')
-    print(f'Time for the grid search: {time.time()-start} s')
+            list_candidates.append(dict_candidate) # Add to list of candidates
+            
+            ###### Create and train the candidate #############################
+
+            candidate = model(**dict_candidate['model'])
+            if verbose: # Print only if verbose
+                str_param = f"Train parameters: {dict_candidate['train']}"
+                print(f"{i} on {n_candidates}: Model {mod_name} --> " + str_param)
+
+            # Train the candidate with the parameters in dict_params['train']
+            candidate.train(input_data, labels, val_data, val_labels, 
+                            **dict_candidate['train'])
+            # predict on validation data
+            pred_val = candidate.predict(val_data)
+            # add the error to the list of error to position i
+            err_val_array[i] = error(val_labels, pred_val)
+            print('\n')
+            i += 1
+
+    if verbose: print(f'Time for the grid search: {time.time()-start} s')
 
     # Sort the errors and return the index of the sorting 
-    order_validation = np.argsort(err_val_array)
+    idx_sorted_val = np.argsort(err_val_array)
 
-    for i in range(n):
+    for i in range(len(list_candidates)):
         # Extract the index using the sorted array of index
-        real_idx = order_validation[i]
+        real_idx = idx_sorted_val[i]
         # Add to the candidate the relative error
         list_candidates[real_idx]['Error'] = err_val_array[real_idx]
         # Add to the array of results the candidate in the chart order
