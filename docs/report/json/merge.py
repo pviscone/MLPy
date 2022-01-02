046fddb84704f26db21db58e59aa454c34b46e42
dodogabrie <dodogabrie97@live.it>
2022-01-02 16:19:58 +0100

    fixed dir bug in merge

--- docs/report/json/merge.py
+++ docs/report/json/merge.py
@@ -1,7 +1,12 @@
 import numpy as np
 import json
 
-mypath = './'
+
+file_name = __file__.split('/')[-1]
+final_dir = __file__[:-len(file_name)]
+
+mypath = final_dir
+
 from os import listdir
 from os.path import isfile, join
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
@@ -29,8 +34,5 @@ for i in range(len(all_dict)):
     idx = sort_err_idx[i]
     final_dict_list.append(all_dict[idx])
 
-file_name = __file__.split('/')[-1]
-final_dir = __file__[:-len(file_name)]
-
-with open(final_dir + 'all_dict.json','w') as fp:
+with open(mypath + 'all_dict.json','w') as fp:
     json.dump(final_dict_list,fp)
