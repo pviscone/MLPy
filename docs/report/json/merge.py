bd3193cd32f7f471ce79925152bc410781c04ea0
dodogabrie <dodogabrie97@live.it>
2022-01-02 16:30:59 +0100

    added fancy print stats

--- docs/report/json/merge.py
+++ docs/report/json/merge.py
@@ -11,14 +11,14 @@ from os import listdir
 from os.path import isfile, join
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 onlyfiles.append('all_dict.json')
-json_files = [f for f in onlyfiles if f.endswith('.json') and f != 'all_dict.json']
+list_exclude = ['all_dict.json']
+json_files = [f for f in onlyfiles if f.endswith('.json') and f not in list_exclude]
 
 all_dict = []
 for f in json_files:
     print(f)
     with open(mypath + f) as json_file:
         f_dict = json.load(json_file)
-    print(len(f_dict))
     for d in f_dict:
         all_dict.append(d)
 
@@ -36,3 +36,12 @@ for i in range(len(all_dict)):
 
 with open(mypath + 'all_dict.json','w') as fp:
     json.dump(final_dict_list,fp)
+
+
+
+print('AND THE WINNER IS.......')
+first_n = 10
+for i in range(first_n):
+    print('-', final_dict_list[i]['model']['structure'])
+    print('-', final_dict_list[i]['model']['func'])
+    print('error:', final_dict_list[i]['Error'])
